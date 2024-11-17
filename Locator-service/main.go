29c904f401937ea9db9b88591c1cca0e5ea62685
andromeda <andreybykovs@yandex.ru>
2024-11-17 23:38:31 +0300

    Обработать ошибки

--- Locator-service/main.go
+++ Locator-service/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -29,26 +28,43 @@ type System struct {
 func serviceHEALTH(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
 	if err != nil {
-		fmt.Fprintf(w, "wrong service id")
+		err = fmt.Errorf("Error while parsing ID from request: %w", err)
+		fmt.Fprintln(w, err)
 		return
 	}
 
 	url := adresses[id] + "/health"
 	service_health := System{}
-	getJson(url, &service_health)
+
+	j_err := getJson(url, &service_health)
+	if j_err != nil {
+		j_err = fmt.Errorf("Error while getting json with system info: %w", j_err)
+		fmt.Fprintln(w, j_err)
+		return
+	}
+
 	data, err := json.Marshal(service_health)
 	if err != nil {
-		fmt.Fprintf(w, "ERROR: marshling ", err)
+		err = fmt.Errorf("Error while marshling json: %w", err)
+		fmt.Fprintln(w, err)
+		return
 	}
+	
 	fmt.Fprintf(w, string(data))
 }
 
 func getJson(url string, target interface{}) error {
 	r, err := myClient.Get(url)
 	if err != nil {
-		return nil
+		return fmt.Errorf("Error while getting URL from client: %w", err)
 	}
-	defer r.Body.Close()
+	defer func() {
+		def_err := r.Body.Close()
+		if def_err != nil {
+			def_err = fmt.Errorf("Error while closing response body: %w", def_err)
+			fmt.Println(def_err)
+		}
+	}()
 	return json.NewDecoder(r.Body).Decode(target)
 }
 
@@ -57,6 +73,7 @@ func main() {
 
 	err := http.ListenAndServe(ip_port, nil)
 	if err != nil {
-		log.Fatal(err)
+		err = fmt.Errorf("Cant ListenAndServe: %w", err)
+		fmt.Println(err)
 	}
 }
